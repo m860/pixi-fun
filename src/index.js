7f9ad9283a43d9bcf4dedda86b71ba0b45cbcac8
jean <jean.ma.1986@gmail.com>
2017-01-11 17:44:12 +0800

    fix

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@
 require("./index.sass");
 import ballImage from './assets/temp/ball.png';
 import brickImage from './assets/temp/brick.png';
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // box2d short name
 let b2BodyDef = Box2D.Dynamics.b2BodyDef,
@@ -24,6 +25,25 @@ let Container = PIXI.Container,
 	resources = PIXI.loader.resources,
 	Sprite = PIXI.Sprite;
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//config
+let config = {
+	debug: true,
+	world: {
+		gravity: new b2Vec2(0, 0),
+		allowSleeping: true,
+		scale: 30.0,
+		positionIterations: 8,
+		velocityIterations: 10,
+		fps: 30.0
+	},
+	renderer: {
+		width: window.innerWidth,
+		height: window.innerHeight
+	}
+}
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 let createRender = (width, height, debug = false)=> {
 	let renderer;
 	if (debug) {
@@ -38,7 +58,7 @@ let appendDebugRender = (world, context, scale = 30.0)=> {
 	let debugDraw = new b2DebugDraw();
 	debugDraw.SetSprite(context);
 	debugDraw.SetAlpha(0.5);
-	debugDraw.SetFlags(b2DebugDraw.e_shapeBit | b2DebugDraw.e_centerOfMassBit);
+	debugDraw.SetFlags(b2DebugDraw.e_shapeBit /*| b2DebugDraw.e_centerOfMassBit*/);
 	debugDraw.SetDrawScale(scale);
 	world.SetDebugDraw(debugDraw);
 };
@@ -58,11 +78,9 @@ let applyState = (body, scale = 30.0)=> {
 	}
 };
 
-let update = (debug, world, scale = 30.0, renderer, stage, fps = 60.0,onKeyboard)=> {
-	if(onKeyboard){
-		onKeyboard();
-	}
-	world.Step(1 / fps, 10, 8);
+let update = (debug, world, scale = 30.0, renderer, stage, fps = 60.0, onKeyboard, lastUpdateTime = Date.now())=> {
+
+	world.Step(1 / fps, config.world.velocityIterations, config.world.positionIterations);
 	if (debug) {
 		world.DrawDebugData();
 	}
@@ -74,8 +92,12 @@ let update = (debug, world, scale = 30.0, renderer, stage, fps = 60.0,onKeyboard
 		}
 		renderer.render(stage);
 	}
+	if (onKeyboard) {
+		onKeyboard(Date.now() - lastUpdateTime);
+	}
+	lastUpdateTime = Date.now();
 	return requestAnimationFrame(()=> {
-		update(debug, world, scale, renderer, stage, fps,onKeyboard);
+		update(debug, world, scale, renderer, stage, fps, onKeyboard, lastUpdateTime);
 	});
 };
 
@@ -99,7 +121,7 @@ let createGround = (world, stage, texture, x, y, width, height, scale = 30.0)=> 
 	body.$sprite = sprite;
 	return body;
 };
-let createBall=(world, stage, texture, x, y, radius, scale = 30.0)=>{
+let createBall = (world, stage, texture, x, y, radius, scale = 30.0)=> {
 	let bodyDef = new b2BodyDef();
 	bodyDef.type = b2Body.b2_dynamicBody;
 	bodyDef.position.Set(getRV(x, scale), getRV(y, scale));
@@ -107,103 +129,247 @@ let createBall=(world, stage, texture, x, y, radius, scale = 30.0)=>{
 	fixtureDef.density = 1.0;
 	fixtureDef.friction = 0.8;
 	fixtureDef.restitution = 0.0;
-	fixtureDef.shape = new b2CircleShape(getRV(radius,scale));
+	fixtureDef.shape = new b2CircleShape(getRV(radius, scale));
 	let body = world.CreateBody(bodyDef);
 	body.CreateFixture(fixtureDef);
 	// add sprite to ground
-	let sprite = new Sprite(texture,x,y);
+	let sprite = new Sprite(texture, x, y);
 	sprite.anchor.set(0.5);
 	stage.addChild(sprite);
 	body.$sprite = sprite;
 	return body;
 };
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-class Keyboard{
-	static W=87
-	static S=83
-	static A=65
-	static D=68
-	static J=74
-	static K=75
-	static SPACE=32
-	constructor(){
-		this.status=new Array(255);
-		this.reset();
-		document.addEventListener('keydown',event=>{
-			this.status[event.keyCode]=true;
-		},false);
-		document.addEventListener('keyup',event=>{
-			this.status[event.keyCode]=false;
-		},false);
+let createEdge = (world, stage, position = 0)=> {
+	let x, y, width, height;
+	switch (position) {
+		case 1:
+			width = 20.0;
+			height = config.renderer.height;
+			x = config.renderer.width + width / 2;
+			y = height / 2;
+			break;
+		case 2:
+			width = config.renderer.width;
+			height = 20;
+			x = width / 2;
+			y = config.renderer.height + height / 2;
+			break;
+		case 3:
+			width = 20.0;
+			height = config.renderer.height;
+			x = -width / 2;
+			y = height / 2;
+			break;
+		default:
+			width = config.renderer.width;
+			height = 20.0;
+			x = width / 2;
+			y = -height / 2;
 	}
-	keydown(code){
+	let bodyDef = new b2BodyDef();
+	bodyDef.type = b2Body.b2_staticBody;
+	bodyDef.position.Set(getRV(x, config.world.scale), getRV(y, config.world.scale));
+	let fixtureDef = new b2FixtureDef();
+	fixtureDef.density = 1.0;
+	fixtureDef.friction = 0.8;
+	fixtureDef.restitution = 0.0;
+	fixtureDef.shape = new b2PolygonShape();
+	fixtureDef.shape.SetAsBox(getRV(width / 2, config.world.scale), getRV(height / 2, config.world.scale));
+	let body = world.CreateBody(bodyDef);
+	body.CreateFixture(fixtureDef);
+	return body;
+};
+let createPlayer = (world)=> {
+	let x = config.renderer.width / 2,
+		y = config.renderer.height - 100,
+		radius = 30;
+	let bodyDef = new b2BodyDef();
+	bodyDef.type = b2Body.b2_dynamicBody;
+	bodyDef.position.Set(getRV(x, config.world.scale), getRV(y, config.world.scale));
+	let fixtureDef = new b2FixtureDef();
+	fixtureDef.density = 1.0;
+	fixtureDef.friction = 0.8;
+	fixtureDef.restitution = 0.0;
+	fixtureDef.shape = new b2CircleShape(getRV(radius, config.world.scale));
+	let body = world.CreateBody(bodyDef);
+	body.SetSleepingAllowed(false);
+	body.CreateFixture(fixtureDef);
+	return body;
+}
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+class Keyboard {
+	static W = 87
+	static S = 83
+	static A = 65
+	static D = 68
+	static J = 74
+	static K = 75
+	static SPACE = 32
+
+	constructor() {
+		this.status = new Array(255);
+		this.reset();
+		document.addEventListener('keydown', event=> {
+			this.status[event.keyCode] = true;
+		}, false);
+		document.addEventListener('keyup', event=> {
+			this.status[event.keyCode] = false;
+		}, false);
+	}
+
+	keydown(code) {
 		return this.status[code];
 	}
-	reset(){
-		for(let i =0;i<this.status.length;i++){
-			this.status[i]=false;
+
+	reset() {
+		for (let i = 0; i < this.status.length; i++) {
+			this.status[i] = false;
+		}
+	}
+}
+class Player {
+	constructor(world) {
+		let x = config.renderer.width / 2,
+			y = config.renderer.height - 100,
+			radius = 25;
+		let bodyDef = new b2BodyDef();
+		bodyDef.type = b2Body.b2_dynamicBody;
+		bodyDef.position.Set(getRV(x, config.world.scale), getRV(y, config.world.scale));
+		let fixtureDef = new b2FixtureDef();
+		fixtureDef.density = 1.0;
+		fixtureDef.friction = 0.8;
+		fixtureDef.restitution = 0.0;
+		fixtureDef.shape = new b2CircleShape(getRV(radius, config.world.scale));
+		let body = world.CreateBody(bodyDef);
+		body.SetSleepingAllowed(false);
+		// body.SetAngle(Math.PI/2);
+		body.CreateFixture(fixtureDef);
+
+		this.world = world;
+		this.body = body;
+		this.angleVelcoityValue = 0.0005;
+		this.forceValue = 0.5;
+
+		this.bulletBodyDef = new b2BodyDef();
+		this.bulletBodyDef.type = b2Body.b2_dynamicBody;
+		this.bulletBodyDef.bullet = true;
+		this.bulletBodyDef.fixedRotation = true;
+		// this.bulletBodyDef.linearVelocity=new b2Vec2()
+		this.bulletFixtureDef = new b2FixtureDef();
+		this.bulletFixtureDef.shape = new b2CircleShape(getRV(2));
+		// this.bulletFixtureDef.shape.SetAsBox(getRV(0.1,config.world.scale),getRV(5,config.world.scale));
+		this.fireDuration = 100;
+		this.lastFireTime = Date.now();
+	}
+
+	turn(deltaTime, clockwise = true) {
+		let angle = this.body.GetAngle();
+		if (!clockwise) {
+			angle -= deltaTime * this.angleVelcoityValue;
+		}
+		else {
+			angle += deltaTime * this.angleVelcoityValue;
+		}
+		this.body.SetAngle(angle);
+	}
+
+	getFacing() {
+		return this.body.GetWorldVector(new b2Vec2(0, -1)).Copy();
+	}
+
+	getWorldFacing() {
+		let v= this.body.GetWorldPoint(new b2Vec2(0,-1)).Copy();
+		v.Multiply(config.world.scale);
+		return v;
+	}
+
+	move(deltaTime, reverse = false) {
+		let force = this.getFacing();
+		if (reverse) {
+			force.Multiply(this.forceValue * deltaTime * -1);
+		}
+		else {
+			force.Multiply(this.forceValue * deltaTime);
+		}
+		this.body.ApplyForce(force, this.body.GetWorldCenter());
+	}
+
+	getCenter() {
+		let pos = this.body.GetPosition().Copy();
+		pos.Multiply(config.world.scale);
+		return pos;
+	}
+
+	drawFacing(context) {
+		let center = this.getCenter();
+		let facingPoint=this.getWorldFacing()
+		// context.beginPath();
+		// context.arc(center.x,center.y,5,0,Math.PI*2);
+		// context.stroke();
+		// context.beginPath();
+		// context.arc(facingPoint.x,facingPoint.y,5,0,Math.PI*2);
+		// context.stroke();
+		context.beginPath();
+		context.moveTo(center.x,center.y);
+		context.lineTo(facingPoint.x,facingPoint.y);
+		context.stroke();
+	}
+
+	fire(deltaTime) {
+		let escape=Date.now()-this.lastFireTime;
+		if(escape>=this.fireDuration) {
+			let bullet = this.world.CreateBody(this.bulletBodyDef);
+			bullet.CreateFixture(this.bulletFixtureDef);
+			let pos = this.getWorldFacing();
+			pos.Multiply(1/config.world.scale);
+			bullet.SetPosition(pos);
+			let velocity = this.getFacing();
+			velocity.Multiply(deltaTime * 0.2);
+			bullet.SetLinearVelocity(velocity);
+			this.lastFireTime=Date.now();
 		}
 	}
 }
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//create canvas render
-let width = 800;
-let height = 600;
-let debug = true;
-// let debug = false;
-let renderer = createRender(width, height, debug);
+//create render
+let renderer = createRender(config.renderer.width, config.renderer.height, config.debug);
+let context = renderer.view.getContext('2d');
 document.body.appendChild(renderer.view);
 let stage = new Container();
 
-let gravity = new b2Vec2(0, 20);
-let allowSleep = true;
-let scale = 30.0;
-let world = new b2World(gravity, allowSleep);
-if (debug) {
-	let context = renderer.view.getContext('2d');
-	appendDebugRender(world, context, scale);
+let world = new b2World(config.world.gravity, config.world.allowSleeping);
+if (config.debug) {
+	appendDebugRender(world, context, config.world.scale);
 }
-let keyboard=new Keyboard();
-loader.add([brickImage,ballImage]).load(()=> {
-	// bottom ground
-	createGround(world,stage,resources[brickImage].texture,400,590,800,20);
-	// left ground
-	createGround(world,stage,resources[brickImage].texture,0,300,20,800);
-	// right ground
-	createGround(world,stage,resources[brickImage].texture,790,300,20,800);
-	//
-	createGround(world,stage,resources[brickImage].texture,400,10,800,20);
+let keyboard = new Keyboard();
 
-	//ball
-	let ball=createBall(world,stage,resources[ballImage].texture,400,300,30);
-	ball.SetSleepingAllowed(false);
-	// ball.ApplyForce(new b2Vec2(2,0),ball.GetLocalCenter());
+loader.add([brickImage, ballImage]).load(()=> {
+	createEdge(world, stage, 0);
+	createEdge(world, stage, 1);
+	createEdge(world, stage, 2);
+	createEdge(world, stage, 3);
 
-	// document.addEventListener("keypress",event=>{
-	// 	if(event.keyCode===32){
-	// 		ball.ApplyForce(new b2Vec2(0,-3),ball.GetWorldCenter());
-	// 	}
-	// },false);
+	let player = new Player(world);
 
 	// loop
-	update(debug, world, scale, renderer, stage, 30,()=>{
-		// let ballPos=ball.GetPosition();
-		// if(keyboard.keydown(Keyboard.W)){
-		// 	ballPos.y--;
-		// }
-		// if(keyboard.keydown(Keyboard.S)){
-		// 	ballPos.y++;
-		// }
-		// if(keyboard.keydown(Keyboard.A)){
-		// 	ballPos.x--;
-		// }
-		// if(keyboard.keydown(Keyboard.D)){
-		// 	ballPos.x++;
-		// }
-		// ball.SetPosition(ballPos);
-		if(keyboard.keydown(Keyboard.SPACE)){
-			ball.SetLinearVelocity(new b2Vec2(0,-10));
+	update(config.debug, world, config.world.scale, renderer, stage, config.world.fps, (deltaTime)=> {
+		world.ClearForces();
+		if (keyboard.keydown(Keyboard.A)) {
+			player.turn(deltaTime, false);
 		}
+		if (keyboard.keydown(Keyboard.D)) {
+			player.turn(deltaTime, true);
+		}
+		if (keyboard.keydown(Keyboard.W)) {
+			player.move(deltaTime, false);
+		}
+		if (keyboard.keydown(Keyboard.S)) {
+			player.move(deltaTime, true);
+		}
+		if (keyboard.keydown(Keyboard.J)) {
+			player.fire(deltaTime);
+		}
+		player.drawFacing(context);
 	});
 });
 
